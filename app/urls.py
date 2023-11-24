835a102cc68472623713b0ff1a3cc17af9651dfb
sadyr7 <sadyr.top@gmail.com>
2023-11-24 20:43:12 +0600

    Добавлена новая функциональность

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,6 @@ router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/books/<int:pk>/get_book_by_id/', BookViewSet.as_view({'get': 'get_book_by_id'}), name='get_book_by_id'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import *
 from .serializers import BookSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    @action(detail=True, methods=['GET'])
+    def get_book_by_id(self, request, pk=None):
+        try:
+            book = Book.objects.get(pk=pk)
+            serializer = self.get_serializer(book)
+            return Response(serializer.data)
+        except Book.DoesNotExist:
+            return Response({'detail': 'Book not found.'}, status=404)
